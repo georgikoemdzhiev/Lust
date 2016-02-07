1f62723fa13b20c8244fe14c54b7bd97c1728350
boggogo <koemdjiev@gmail.com>
2016-02-07 18:26:38 +0000

    - change the color of the background floating button menus.

--- app/src/main/java/com/studentsins/lust/MainActivity.java
+++ app/src/main/java/com/studentsins/lust/MainActivity.java
@@ -91,6 +91,7 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+
             return true;
         }
         if(id == R.id.action_logout){
