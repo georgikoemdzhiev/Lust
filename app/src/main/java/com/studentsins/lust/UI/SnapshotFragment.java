88c68ad830286515953dec16c5a108b3ae029a0c
boggogo <koemdjiev@gmail.com>
2016-03-02 15:58:42 +0000

    - Changed the question mark for the cant device circle - UI Adjustments for the snapshot screen

--- app/src/main/java/com/studentsins/lust/UI/SnapshotFragment.java
+++ app/src/main/java/com/studentsins/lust/UI/SnapshotFragment.java
@@ -78,7 +78,7 @@ public class SnapshotFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_snapshot_layout,container,false);
-        mCantDecideGg = (ImageView)view.findViewById(R.id.cant_devide_black_circle_bg);
+        mCantDecideGg = (ImageView)view.findViewById(R.id.cant_decide_black_circle_bg);
 
         mCantDecideGg.setOnTouchListener(onTouchListener);
         mCantDecideProgressBar = (CircleProgressBar)view.findViewById(R.id.cantDecideProgressBar);
