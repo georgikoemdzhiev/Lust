cdd773cfb6d7f5d58b354ae20bd86d9b94182878
boggogo <koemdjiev@gmail.com>
2016-03-05 09:14:00 +0000

    - Added comments in the LustApplication class

--- app/src/main/java/com/studentsins/lust/LustApplication.java
+++ app/src/main/java/com/studentsins/lust/LustApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 /**
+ * Application class to handle the local database setup - to be added later on.
  * Created by Georgi on 1/18/2016.
  */
 public class LustApplication extends Application {
