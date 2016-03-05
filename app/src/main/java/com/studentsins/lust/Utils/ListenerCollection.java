11fbb555297a430dc7691d2bcb83d2858e3c3b17
boggogo <koemdjiev@gmail.com>
2016-03-05 09:13:01 +0000

    - Added comments in the ListenerCollection class

--- app/src/main/java/com/studentsins/lust/Utils/ListenerCollection.java
+++ app/src/main/java/com/studentsins/lust/Utils/ListenerCollection.java
@@ -7,12 +7,14 @@ import android.util.Log;
 import com.studentsins.lust.UI.MainActivity;
 
 /**
+ * Class to hold any listeners (whenever possible) needed elsewhere in the app.
+ * The purpose of doing this is to make the code spread in different classes and thus more readable.
  * Created by koemdzhiev on 11/02/16.
  */
 public class ListenerCollection {
     private static final String TAG = ListenerCollection.class.getSimpleName();
     private static boolean isFabShown = true;
-
+//Listener to handle the showing/hiding of the floating action button - FAB
     public  static RecyclerView.OnScrollListener showHideFAB = new RecyclerView.OnScrollListener() {
         @Override
         public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
@@ -47,20 +49,21 @@ public class ListenerCollection {
 //            Log.d(TAG,"onPageScrollStateChanded");
         }
     };
-
+//Helper method to hide the FAB...
     private static void hideFAB() {
         MainActivity.mFloatingActionsMenu.animate()
                 .setDuration(150)
                 .translationY(300);
         isFabShown = false;
     }
-
+//Helper method to show the FAB...
     public static void showFAB() {
         MainActivity.mFloatingActionsMenu.animate()
                 .setDuration(200)
                 .translationY(0);
         isFabShown = true;
     }
+//    Method to expand the toolbar. This is not used since it is not needed at the moment.
     public static void expandToolbar(){
         //setExpanded(boolean expanded, boolean animate)
         MainActivity.appBarLayout.setExpanded(true, true);
